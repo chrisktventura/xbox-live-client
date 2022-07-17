c87e33473e6b731dd73d5d6601164c3ebb047069
Christopher Ventura <chrisventura@outlook.com.br>
2022-07-17 20:39:31 -0300

    ajuste bg

--- app/src/components/BgDefault/index.tsx
+++ app/src/components/BgDefault/index.tsx
@@ -5,137 +5,38 @@ import * as S from './style';
 
 const BgDefault = () => {
     const iconArray: Array<any> = Array.apply(null, Array(Math.floor(window.innerHeight / 64))).map(Number.call, Number);
+    const tempIcon: Array<any> = [faBookDead, faDragon, faDungeon, faGhost, faHatCowboy, faHatWizard, faRing, faScroll, faChessRook, faHeart, faShieldHalved, faSkullCrossbones, faPuzzlePiece, faGamepad, faLeaf, faCircleRadiation, faBasketball, faTrophy, faCompass, faBicycle, faBomb, faCannabis, faCarSide, faCube, faBacterium];
+    const randomIcon = () => {
+        var min = Math.ceil(0);
+        var max = Math.floor(tempIcon.length - 1);
+        var lastIcon: any = "null";
+        const resultArray: Array<any> = [];
+        iconArray.forEach((item, index) => {
+            var temp = tempIcon[Math.floor(Math.random() * (max - min + 1)) + min]
+            if (lastIcon != temp) {
+                var blah =
+                        <S.iconBgHover>
+                            <S.IconBg>
+                                <FontAwesomeIcon icon={temp as IconProp} />
+                            </S.IconBg>
+                        </S.iconBgHover>
+                resultArray.push(blah)
+                lastIcon = temp;
+            }
+        })
+        return resultArray
+
+    }
+    const tempTemp: Array<any> = randomIcon();
+
     return (
         <S.BgDefaultContent>
-            {iconArray.map((item, index) => {
-                return <S.BgDefaultRow>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faDragon as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faBookDead as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faDungeon as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faGhost as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faHatCowboy as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faHatWizard as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faRing as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faScroll as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faChessRook as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faHeart as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faShieldHalved as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faSkullCrossbones as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faPuzzlePiece as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faGamepad as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faLeaf as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faCircleRadiation as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faBasketball as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faCannabis as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faTrophy as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faCompass as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faBicycle as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faBomb as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faCarSide as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faCube as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                    <S.iconBgHover>
-                        <S.IconBg>
-                            <FontAwesomeIcon icon={faBacterium as IconProp} />
-                        </S.IconBg>
-                    </S.iconBgHover>
-                </S.BgDefaultRow>
+                    <S.BgDefaultRow>
+            
+            {tempTemp.map((item, index) => {
+                return item
             })}
+                    </S.BgDefaultRow>
 
 
         </S.BgDefaultContent>
