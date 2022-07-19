b1c453f4a59afc0b6c8ab343840537f3f84afbdd
Christopher Ventura <chrisventura@outlook.com.br>
2022-07-19 19:44:22 -0300

    feat gamecard

--- app/src/components/GameCard/index.tsx
+++ app/src/components/GameCard/index.tsx
@@ -1,12 +1,24 @@
 import * as S from './style'
-
 import React from 'react'
+
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const GameCard = () => {
   return (
-    <S.GameCard>
-        
-    </S.GameCard>
+    <S.GameCardContent>
+      
+      <S.GameCardImage src='https://i.ytimg.com/vi/TeO3Vaju8lg/sddefault.jpg'/>
+
+      <S.GameCardTitle>Street Chaves</S.GameCardTitle>
+
+      <S.GameCardYear>2019</S.GameCardYear>
+
+      <S.GameCardImdb>4</S.GameCardImdb>
+
+      
+    
+    </S.GameCardContent>
   )
 }
 
--- app/src/components/GameCard/style.ts
+++ app/src/components/GameCard/style.ts
@@ -1,8 +1,44 @@
 import styled, { css } from 'styled-components';
 
-export const GameCard = styled.div`
+export const GameCardContent = styled.div`
     ${({ theme }) => css`
-    
-    `};
+        
+        flex-direction: column;
+        width: 18rem;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+        text-align: center;
+        padding: 0.5rem;
+        background-color: white;
 
+    `};
+`
+
+export const GameCardImage = styled.img`
+    width: 100%;
+    height: 180px;
+    object-fit: cover;
+    object-position: center;
+    
+`
+
+export const GameCardTitle = styled.h1`
+    ${({ theme }) => css`
+    font-size: 1.4rem;
+    font-weight: bold;
+    color: ${theme.colors.iconColor1};
+    `}
+`
+
+export const GameCardYear = styled.p`
+    font-size: 1.2rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.iconColor1};
+`
+
+export const GameCardImdb = styled.p`
+    font-size: 1.2rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.iconColor1};
 `
