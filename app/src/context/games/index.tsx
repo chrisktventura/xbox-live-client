970ce73581dc80a23f4edd057b4d9582a559fad4
Christopher Ventura <chrisventura@outlook.com.br>
2022-08-14 19:12:24 -0300

    feature game context

---
+++ app/src/context/games/index.tsx
@@ -0,0 +1,57 @@
+import {
+    createContext,
+    useContext,
+    ReactNode,
+    useState,
+    useEffect,
+  } from "react";
+  import { Game } from "../../types/gametypes";
+  import { api } from "../../services/api";
+  import { useAuth } from "../auth";
+  
+  interface GameProviderProps {
+    children: ReactNode;
+  }
+  
+  interface GameProviderData {
+    games: Game[];
+    handleGetGames: () => void;
+  }
+  
+  const GamesContext = createContext<GameProviderData>(
+    {} as GameProviderData
+  );
+  
+  export const ProductsProvider = ({ children }: GameProviderProps) => {
+    const { logged } = useAuth();
+  
+    const [games, setGame] = useState<Game[]>([]);
+  
+    const handleGetGames = () => {
+      const token = localStorage.getItem("token");
+  
+      const headers = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+  
+      api.get("/games", headers).then((res) => {
+        console.log(res);
+        setGame(res.data);
+      });
+    };
+  
+    useEffect(() => {
+      if (logged) handleGetGames();
+    }, [logged]);
+  
+    return (
+      <GamesContext.Provider value={{ games, handleGetGames }}>
+        {children}
+      </GamesContext.Provider>
+    );
+  };
+  
+  export const useProducts = () => useContext(GamesContext);
+  
