0faac9e1e434e3cabe8dafda56b3e77542393631
Christopher Ventura <chrisventura@outlook.com.br>
2022-08-20 13:53:26 -0300

    mocks temporarios

--- app/src/mocks/profile.ts
+++ app/src/mocks/profile.ts
@@ -1,1 +1,44 @@
-export {}
+import * as T from '../types/profiletypes';
+
+export const mockedProfiles: T.Profile[] = [
+    {
+        id: '1',
+        name: 'teste',
+        imageURL: '',
+        userId: 'string',
+        gameId: 'string',
+        favoritos: 'string',
+    },
+    {
+        id: '2',
+        name: 'teste',
+        imageURL: '',
+        userId: 'string',
+        gameId: 'string',
+        favoritos: 'string',
+    },
+    {
+        id: '3',
+        name: 'teste',
+        imageURL: '',
+        userId: 'string',
+        gameId: 'string',
+        favoritos: 'string',
+    },
+    {
+        id: '4',
+        name: 'teste',
+        imageURL: '',
+        userId: 'string',
+        gameId: 'string',
+        favoritos: 'string',
+    },
+    {
+        id: '5',
+        name: 'teste',
+        imageURL: '',
+        userId: 'string',
+        gameId: 'string',
+        favoritos: 'string',
+    },
+]
---
+++ app/src/mocks/users.ts
@@ -0,0 +1,13 @@
+import * as T from '../types/usertypes';
+
+export const mockedUsers: T.User[] = [
+    {
+        id: '1',
+        nickname: 'teste1',
+        email: 'teste1@teste.com',
+        password: 'string',
+        confirmPassword: 'string',
+        cpf: 'string',
+        isAdmin: false,
+    }
+]
