1db255b572035a14b68c0c00919d18958c6cdeb2
Christopher Ventura <chrisventura@outlook.com.br>
2022-08-19 21:11:37 -0300

    ajuste game list

---
+++ app/src/components/GameList/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import * as S from './style'
+import { Game } from '../../types/gametypes'
+import GameCard from 'components/GameCard';
+
+interface GamesListProps {
+    game: Game[];
+}
+
+const GamesList = ({ game }: GamesListProps) => {
+  return (
+    <S.GameListContainer>
+        {game.map((element) => (
+            <GameCard game={element} key={element.id} />
+        ))}
+    </S.GameListContainer>
+  )
+}
+
+export default GamesList;
---
+++ app/src/components/GameList/style.ts
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components'
+
+export const GameListContainer = styled.div`
+    ${({ theme }) => css`
+    width: 100%;
+    height: 100vh
+
+    
+    `}
+`
