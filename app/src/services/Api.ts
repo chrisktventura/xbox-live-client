d146d685273199f9853c85133199ee9212edd005
Christopher Ventura <chrisventura@outlook.com.br>
2022-08-19 21:12:27 -0300

    ajuste api

--- app/src/services/Api.ts
+++ app/src/services/Api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://xbox-live-server-blue.onrender.com/",
+  baseURL: "https://xbox-live-server-blue.onrender.com/"
 });
