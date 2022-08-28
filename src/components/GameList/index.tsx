9df4d3d766d6df6618058f45a451ce693c98efbf
Christopher Ventura <chrisventura@outlook.com.br>
2022-08-28 13:06:44 -0300

    ajustes na gamelist

--- src/components/GameList/index.tsx
+++ src/components/GameList/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
 import * as S from './style'
 import { Game } from '../../types/gametypes'
 import GameCard from 'components/GameCard';
 
 interface GamesListProps {
     game: Game[];
-}
+    isFavoritesList: boolean;
+    handleGetFavorites: () => void;
+  }
 
-const GamesList = ({ game }: GamesListProps) => {
+const GamesList = ({ 
+  game,
+  isFavoritesList,
+  handleGetFavorites,
+ }: GamesListProps) => {
   return (
     <S.GameListContainer>
         {game.map((element) => (
-            <GameCard game={element} key={element.id} />
+            <GameCard
+            game={element} 
+            key={element.id} 
+            isFavoritesList={isFavoritesList}
+            handleGetFavorites={handleGetFavorites}            
+            />
         ))}
     </S.GameListContainer>
   )
